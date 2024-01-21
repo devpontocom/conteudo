b248f22f23055739f4c4ae0f3c48d8ef1bea88cd
Jonas Ferreira <jonas.silva@grupomateus.com>
2024-01-21 16:28:00 -0300

    ajustando texto

--- exemplos/iniciantes/snippets/gerador_de_qrcode.py
+++ exemplos/iniciantes/snippets/gerador_de_qrcode.py
@@ -2,14 +2,14 @@
 
 import qrcode
 
-dados = "https://www.devponto.com"
+conteudo_do_qrcode = "https://www.devponto.com"
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=10,
     border=4,
 )
-qr.add_data(dados)
+qr.add_data(conteudo_do_qrcode)
 qr.make(fit=True)
 
 img = qr.make_image(fill_color="black", back_color="white")
